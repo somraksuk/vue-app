3e2c682ef8ec6461756e0be54408a05325bf4104
somraksuk <somrak.suk@gmail.com>
2018-05-23 15:31:31 +0700

    router2

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,31 @@
 <template>
-  <div>About {{name}}
-
+  <div>
+    <h1>Hello Vue</h1>
+    <div>
+      <nuxt-link to="/about">About</nuxt-link>
+    </div>
+       <div>
+<button @click="gotoAbout">GO TO About</button>
+    </div>
   </div>
-</template>
+</template
 <script>
 export default {
-  data(){
-    return{
-      name:'Somrak',
-      age:25,
-    }
+  methods: {
+    gotoAbout() {
+      this.$router.push('/about')
+      this.$router.push({ path: '/about' })
+      this.$router.push('/about?id=1')
+      this.$router.push({ path: '/about', query: {id: 1} })
+      this.$router.replace('/about')
+      this.$router.replace({ path: '/about' })
+      this.$router.replace('/about?id=1')
+      this.$router.replace({ path: '/about', query: {id: 1} })
+      this.$router.go(-1)
+    },
   },
 }
 </script>
+
+
 
---
+++ pages/index1.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>About {{name}}
+
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      name:'Somrak',
+      age:25,
+    }
+  },
+}
+</script>
+
